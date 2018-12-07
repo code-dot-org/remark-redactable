846601298401467a42d4e75a4034ca1b8a2d6492
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-12-07 15:41:31 -0800

    expose redactedLink plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const redact = require('./redact');
 const restore = require('./restore');
+const plugins = require('./plugins');
 
 module.exports = {
   redact,
-  restore
+  restore,
+  plugins,
 }
---
+++ src/plugins/index.js
@@ -0,0 +1,5 @@
+const redactedLink = require('./redactedLink');
+
+module.exports = {
+  redactedLink,
+}
