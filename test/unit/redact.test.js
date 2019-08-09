c95a2eafe379ad43a119b75d3ac2ff2913bec6c9
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-08-09 12:58:12 -0700

    use link plugin from remark-plugins rather than defining here

--- test/unit/redact.test.js
+++ test/unit/redact.test.js
@@ -3,7 +3,7 @@ const tape = require('tape');
 
 const redact = require('../../src/redact');
 const restore = require('../../src/restore');
-const redactedLink = require('../../src/plugins/redactedLink');
+const { link } = require('@code-dot-org/remark-plugins');
 
 const sourceText = "A [black](http://example.com/black) [cat](http://example.com/cat)\n";
 
@@ -22,7 +22,7 @@ tape('redaction', (test) => {
 
   const redactedText = remark()
     .use(redact)
-    .use(redactedLink)
+    .use(link)
     .processSync(sourceText).contents;
 
   test.equal(redactedText, "A [black][0] [cat][1]\n");
@@ -33,14 +33,14 @@ tape('restoration', (test) => {
 
   const redactedSourceTree = remark()
     .use(redact)
-    .use(redactedLink)
+    .use(link)
     .parse(sourceText);
 
   const translatedText = "Une [chat][1] [noir][0]\n";
 
   const restoredText = remark()
     .use(restore(redactedSourceTree))
-    .use(redactedLink)
+    .use(link)
     .processSync(translatedText).contents;
 
   test.equal(restoredText, "Une [chat](http://example.com/cat) [noir](http://example.com/black)\n");
