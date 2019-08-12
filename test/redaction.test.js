b120453f2352767c2e2341ac20e2bf4127c48686
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-08-12 14:32:05 -0700

    add data-driven tests

---
+++ test/redaction.test.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+const remark = require("remark");
+const tape = require("tape");
+
+const { redact } = require("..");
+
+fs.readdirSync(path.resolve(__dirname, "data")).forEach(testCategory => {
+  const dataPath = path.resolve(__dirname, "data", testCategory);
+
+  const plugin = require(path.resolve(dataPath, "plugin.js"));
+  const redactedText = fs.readFileSync(
+    path.resolve(dataPath, "redacted.md"),
+    "utf-8"
+  );
+  const redactedTree = require(path.resolve(dataPath, "redacted.json"));
+  const sourceText = fs.readFileSync(path.join(dataPath, "source.md"), "utf-8");
+
+  tape(testCategory, test => {
+    test.plan(3);
+
+    // string to ast
+    test.test("content can be redacted to Redaction nodes", subtest => {
+      subtest.plan(1);
+      subtest.deepEqual(
+        remark()
+          .use(redact)
+          .use(plugin)
+          .parse(sourceText),
+        redactedTree
+      );
+    });
+
+    // ast to string
+    test.test("Redaction nodes can render to string", subtest => {
+      subtest.plan(1);
+      subtest.equal(
+        remark()
+          .use(redact)
+          .stringify(redactedTree),
+        redactedText
+      );
+    });
+
+    // string to string
+    test.test("content can redact to string", subtest => {
+      subtest.plan(1);
+      subtest.equal(
+        remark()
+          .use(redact)
+          .use(plugin)
+          .processSync(sourceText).contents,
+        redactedText
+      );
+    });
+  });
+});
