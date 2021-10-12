ac298aa49e0d03bbb1a219e639f177ea78f7d072
Anna Xu <annaxu@wustl.edu>
2021-10-12 16:25:13 -0700

    removing consol log statement

--- src/renderRestorations.js
+++ src/renderRestorations.js
@@ -1,16 +1,13 @@
-/* eslint-disable no-console */
 
 module.exports = function renderRestorations() {
   if (this.Compiler) {
     const visitors = this.Compiler.prototype.visitors;
     if (visitors) {
-      console.log("render restorations function");
 
       visitors.inlineRestoration = function(node) {
         if (node.redactionType === 'link' || node.redactionType === 'image') {
           this.enterLink();
         }
-        console.log("render restorations node: " + JSON.stringify(node));
         return `[${node.content}][${node.redactionIndex}]`;
       };
 
