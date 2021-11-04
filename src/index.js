f69ce46ce1d660322fb2d0abda29b30871eca12a
Anna Xu <annaxu@wustl.edu>
2021-11-04 15:17:15 -0700

    remove package.json changes and transformRedactions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ const redact = require('./redact');
 const restore = require('./restore');
 const parseRestorations = require('./parseRestorations');
 const renderRestorations = require('./renderRestorations');
-const transformRedactions = require('./transformRedactions');
 
 module.exports = {
   redact,
   restore,
   parseRestorations,
-  renderRestorations,
-  transformRedactions
+  renderRestorations
 }
